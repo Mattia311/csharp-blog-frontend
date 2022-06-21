8e4453af42446e73b6cb3353ccb315a2de4f1da3
Mattia Villani <mattia.space98@gmail.com>
2022-06-21 11:27:41 +0200

    commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,70 @@ namespace csharp_blog_frontend.Controllers
 {
     public class HomeController : Controller
     {
-       [HttpGet]
-       public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            ViewData["id"] = id;
+
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult DetailBlob(int id)
+        {
+            ViewData["id"] = id;
+
+            return View();
+        }
+
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
-        [HttpGet]
-        public IActionResult Details(int id)
+
+        public IActionResult CreateFile()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
         [HttpGet]
         public IActionResult Update(int id)
         {
             return View();
         }
-       
-       
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
